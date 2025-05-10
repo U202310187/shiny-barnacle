39e09292f82865e11592d5c6f5179e119b566725
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-09 22:57:40 -0500

    feat: add db file

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+    $conn = mysqli_connect(
+        'localhost', # servidor de la base de datos
+        'root', # usuario de la base de datos
+        '', # contraseña de la base de datos
+        'crafted' # nombre de la base de datos
+    );
+
+    if(isset($conn)){
+        echo "Conexión exitosa a la base de datos";
+    } else{
+        echo "Error de conexión a la base de datos: " . mysqli_connect_error();
+    }
+?>
