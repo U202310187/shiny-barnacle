cf7396721f0a680d44bffbf8df462248c20242de
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-09 23:52:28 -0500

    feat: add bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <?php include("db.php")?> <!--Importa conexion DB-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial</title>
+
+    <!---- Bootstrap’s compiled CSS ---->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <!-- Mis estilos personalizados van después del CSS de Bootstrap -->
+</head>
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">Inicio</a>
+        </div>
+    </nav>
+
+    <h1>HOLA MUNDO</h1>
+
+    <!---- Bootstrap’s compiled JavaScript ---->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <!-- Mis scripts personalizados van después de Bootstrap -->
+</body>
+</html>
