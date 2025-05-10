6e02a3b6c222e2b3ac3aef7bc3e9ce49a0179e43
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-05-09 23:43:16 -0500

    feat: add import conexion

--- db.php
+++ db.php
@@ -6,9 +6,9 @@
         'crafted' # nombre de la base de datos
     );
 
-    if(isset($conn)){
+    /*if(isset($conn)){
         echo "Conexión exitosa a la base de datos";
     } else{
         echo "Error de conexión a la base de datos: " . mysqli_connect_error();
-    }
+    }*/
 ?>
